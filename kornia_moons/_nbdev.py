986bd9e339d917963b6fc9f94c3ff78db0a90c3b
Dmytro Mishkin <ducha.aiki@gmail.com>
2021-07-21 15:14:05 +0200

    added doc

--- kornia_moons/_nbdev.py
+++ kornia_moons/_nbdev.py
@@ -10,7 +10,11 @@ index = {"laf_from_opencv_kpts": "feature.ipynb",
          "opencv_SIFT_kpts_from_laf": "feature.ipynb",
          "opencv_ORB_kpts_from_laf": "feature.ipynb",
          "cv2_matches_from_kornia": "feature.ipynb",
-         "kornia_matches_from_cv2": "feature.ipynb"}
+         "kornia_matches_from_cv2": "feature.ipynb",
+         "to_numpy_image": "feature.ipynb",
+         "epilines_to_start_end_points": "feature.ipynb",
+         "draw_LAF_matches": "feature.ipynb",
+         "draw_LAF_matches_from_result_dict": "feature.ipynb"}
 
 modules = ["feature.py"]
 
--- kornia_moons/feature.py
+++ kornia_moons/feature.py
@@ -2,7 +2,8 @@
 
 __all__ = ['laf_from_opencv_kpts', 'visualize_LAF', 'opencv_kpts_from_laf', 'laf_from_opencv_ORB_kpts',
            'laf_from_opencv_SIFT_kpts', 'opencv_SIFT_kpts_from_laf', 'opencv_ORB_kpts_from_laf',
-           'cv2_matches_from_kornia', 'kornia_matches_from_cv2']
+           'cv2_matches_from_kornia', 'kornia_matches_from_cv2', 'to_numpy_image', 'epilines_to_start_end_points',
+           'draw_LAF_matches', 'draw_LAF_matches_from_result_dict']
 
 # Cell
 import cv2
@@ -10,6 +11,8 @@ import torch
 import kornia as K
 from typing import List, Union, Tuple, Optional
 import matplotlib.pyplot as plt
+import numpy as np
+from copy import deepcopy
 
 def laf_from_opencv_kpts(kpts: List[cv2.KeyPoint],
                          mrSize: float=6.0,
@@ -80,3 +83,213 @@ def kornia_matches_from_cv2(cv2_matches, device=torch.device('cpu')):
         match_idxs[i, 0] = m.queryIdx
         match_idxs[i, 1] = m.trainIdx
     return match_dists, match_idxs
+
+# Cell
+from typing import Union
+
+def to_numpy_image(img: Union[str, np.array, torch.Tensor]):
+    if type(img) is str:
+        img_out = cv2.cvtColor(cv2.imread(img), cv2.COLOR_BGR2RGB)
+    elif isinstance(img, torch.Tensor):
+        img_out = K.tensor_to_image(img)
+    elif isinstance(img, np.ndarray):
+        img_out = img
+    else:
+        raise TypeError('img should be str, np.array or torch.Tensor')
+    return img_out
+
+# Cell
+def epilines_to_start_end_points(epi, h, w):
+    num = len(epi)
+    zeros = torch.zeros(num)
+    ones = torch.ones(num)
+    eps = 1e-8
+
+
+    b = -epi[:,2] / (epi[:,1] + eps)
+    k = -epi[:,0] / (epi[:,1] + eps)
+    k_positive = k >= 0
+    k_negative = k < 0
+
+
+    # We find the points of crossing x = 0, x = w, y = 0, y = h
+    p1 = torch.stack([zeros,  b], axis=-1)
+    p2 = torch.stack([w*ones, k*w + b], axis =-1)
+    p3 = torch.stack([-b/(k + eps), zeros], axis=-1)
+    p4 = torch.stack([(h - b)/(k + eps), ones*h], axis=-1)
+
+    # Now we select those, which are inside of image
+    p1_is_start = (p1[:, 1:2] <= h) & (p1[:, 1:2] >= 0)
+    p3_is_start = (k_positive * (~p1_is_start.view(-1))).view(-1,1)
+    p4_is_start = (k_negative * (~p1_is_start.view(-1))).view(-1,1)
+    none_is_start = ~(p1_is_start | p3_is_start |  p4_is_start)
+
+
+    p2_is_fin  = (p2[:, 1:2] <= h) & (p2[:, 1:2] >= 0)
+    p4_is_fin  = (k_positive * (~p2_is_fin.view(-1))).view(-1,1)
+    p3_is_fin  = (k_negative * (~p2_is_fin.view(-1))).view(-1,1)
+
+
+    start_lines = p1 * p1_is_start.float() + p3 * p3_is_start.float() + p4 * p4_is_start.float()
+    fin_lines   = p2 * p2_is_fin.float()   + p4 * p4_is_fin.float() + p3 * p3_is_fin.float()
+    return torch.stack ([start_lines, fin_lines])
+
+# Cell
+import kornia as K
+import kornia.feature as KF
+import torch
+def draw_LAF_matches(lafs1, lafs2, tent_idxs,
+                        img1, img2, inlier_mask = None,
+                        draw_dict={"inlier_color": (0.2, 1, 0.2),
+                               "tentative_color": (0.8, 0.8, 0),
+                               "feature_color": (0.2, 0.5, 1),
+                                  "vertical": False},
+                        Fm: Optional[np.array] = None, H: Optional[np.array] = None,
+                        ax: Optional = None):
+    '''This function draws LAFs, tentative matches, inliers epipolar lines (if F is provided),
+    and image1 corners reprojection into image 2 (if H is provided)'''
+    inlier_mask = np.array(inlier_mask).reshape(-1)
+    img1 = to_numpy_image(img1)
+    img2 = to_numpy_image(img2)
+
+    h,w = img1.shape[:2]
+    h2,w2 = img2.shape[:2]
+
+    corners_pts = np.float32([[0, 0],
+                              [0, h-1],
+                              [w-1, h-1],
+                              [w-1, 0],
+                              [0, 0] ]).reshape(-1,1,2)
+
+
+    img2_tr = deepcopy(img2)
+
+    xy1 = KF.get_laf_center(lafs1).reshape(-1, 2)
+    xy2 = KF.get_laf_center(lafs2).reshape(-1, 2)
+    # If we have no axes, create one
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(20,10))
+
+    tent_cv2 = cv2_matches_from_kornia(torch.ones(len(tent_idxs)), tent_idxs)
+    tent_corrs = torch.stack([xy1[tent_idxs[:,0]], xy2[tent_idxs[:,1]]])
+    try:
+        vert = draw_dict['vertical']
+    except:
+        vert = False
+    if vert:
+        tent_corrs[1,:,1]+=h # shift for the 2nd image
+    else:
+        tent_corrs[1,:,0]+=w # shift for the 2nd image
+    if H is not None:
+        dst_corners = cv2.perspectiveTransform(corners_pts, H)
+        if vert:
+            dst_corners[...,1]+=h
+        else:
+            dst_corners[...,0]+=w
+    # Prepraring canvas
+    if not vert:
+        if len(img1.shape) == 3:
+            new_shape = (max(h, h2), w + w2, img1.shape[2])
+        elif len(img1.shape) == 2:
+            new_shape = (max(h, h2), w + w2)
+    else:
+        if len(img1.shape) == 3:
+            new_shape = (h + h2,  max(w, w2), img1.shape[2])
+        elif len(img1.shape) == 2:
+            new_shape = (h + h2,  max(w, w2))
+    new_img = np.zeros(new_shape, type(img1.flat[0]))
+    # Place images onto the new image.
+    if not vert:
+        new_img[0:h, 0:w] = img1
+        new_img[0:h2, w:w + w2] = img2
+    else:
+        new_img[0:h, 0:w] = img1
+        new_img[h:h+h2, 0:w2] = img2
+
+    x1,y1 = KF.laf.get_laf_pts_to_draw(lafs1, 0)
+    x2,y2 = KF.laf.get_laf_pts_to_draw(lafs2, 0)
+    if vert:
+        y2+=h
+    else:
+        x2+=w
+    # Drawing features
+    try:
+        fc = draw_dict['feature_color']
+    except:
+        fc = None
+    if fc is not None:
+        ax.plot(x1, y1, color=fc)
+        ax.plot(x2, y2, color=fc)
+
+    #Drawgin tentatives
+    try:
+        tc = draw_dict['tentative_color']
+    except:
+        tc = None
+    if tc is not None:
+        ax.plot(tent_corrs[...,0], tent_corrs[...,1], color=tc)
+        ax.plot(x1[:, tent_idxs[:,0]], y1[:, tent_idxs[:,0]], color=tc)
+        ax.plot(x2[:, tent_idxs[:,1]], y2[:, tent_idxs[:,1]], color=tc)
+    try:
+        ic = draw_dict['inlier_color']
+    except:
+        ic = None
+    if (ic is not None) and (inlier_mask is not None):
+        inlier_mask = inlier_mask > 0
+        ax.plot(tent_corrs[..., inlier_mask, 0], tent_corrs[...,inlier_mask, 1], color=ic)
+        ax.plot(x1[:, tent_idxs[inlier_mask,0]], y1[:, tent_idxs[inlier_mask,0]], color=ic)
+        ax.plot(x2[:, tent_idxs[inlier_mask,1]], y2[:, tent_idxs[inlier_mask,1]], color=ic)
+    if H is not None:
+        ax.plot(corners_pts[:,0,0], corners_pts[:,0,1], color=(0,0,1))
+        ax.plot(dst_corners[:,0,0], dst_corners[:,0,1], color=(0,0,1))
+    if (Fm is not None):
+        if inlier_mask is None:
+            inlier_mask = [True for i in range(len(tent_idxs))]
+        tent_corrs2 = torch.stack([xy1[tent_idxs[:,0]], xy2[tent_idxs[:,1]]])
+        inl1 = tent_corrs2[0, inlier_mask]
+        inl2 = tent_corrs2[1, inlier_mask]
+        Ff = torch.from_numpy(Fm).float()
+        epi1 = K.geometry.compute_correspond_epilines(inl2.reshape(1,-1, 2), Ff.t()[None])[0]
+        epiline1 = epilines_to_start_end_points(epi1, h, w)
+
+        epi2 = K.geometry.compute_correspond_epilines(inl1.reshape(1,-1, 2), Ff[None])[0]
+        epiline2 = epilines_to_start_end_points(epi2, h2, w2)
+        if vert:
+            epiline2[...,1]+=h # shift for the 2nd image
+        else:
+            epiline2[...,0]+=w # shift for the 2nd image
+
+        ax.plot(epiline1[:,:,0], epiline1[:,:,1], color='purple')
+        ax.plot(epiline2[:,:,0], epiline2[:,:,1], color='purple')
+    # Finally clip the image
+    ax.imshow(new_img)
+    if not vert:
+        ax.set_xlim([0,w+w2])
+        ax.set_ylim([max(h,h2),0])
+        ax.margins(0,0)
+    else:
+        ax.set_xlim([0,max(w,w2)])
+        ax.set_ylim([h+h2, 0])
+        ax.margins(0,0)
+    return
+
+# Cell
+def draw_LAF_matches_from_result_dict(result_dict,
+                        img1, img2,
+                        draw_dict={"inlier_color": (0.2, 1, 0.2),
+                               "tentative_color": (0.8, 0.8, 0),
+                               "feature_color": (0.2, 0.5, 1),
+                                  "vertical": False}):
+    if 'H' in result_dict:
+        H = result_dict['H']
+    else:
+        H = None
+    if 'F' in result_dict:
+        Fm = result_dict['F']
+    else:
+        Fm = None
+    return draw_LAF_matches(result_dict['feat_dict1']['lafs'],
+                        result_dict['feat_dict2']['lafs'],
+                               result_dict['tents_dict']['idxs'],
+                        img1, img2, result_dict['inlier_mask'],
+                        draw_dict, Fm, H)
