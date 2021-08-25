fd7c0a4639dfffb1fcca3ed84c64070e9a191bce
Dmytro Mishkin <ducha.aiki@gmail.com>
2021-08-25 14:57:58 +0200

    Added function for MODS-like homography drawing

--- kornia_moons/_nbdev.py
+++ kornia_moons/_nbdev.py
@@ -14,7 +14,8 @@ index = {"laf_from_opencv_kpts": "feature.ipynb",
          "to_numpy_image": "feature.ipynb",
          "epilines_to_start_end_points": "feature.ipynb",
          "draw_LAF_matches": "feature.ipynb",
-         "draw_LAF_matches_from_result_dict": "feature.ipynb"}
+         "draw_LAF_matches_from_result_dict": "feature.ipynb",
+         "draw_LAF_inliers_perspective_repjojected": "feature.ipynb"}
 
 modules = ["feature.py"]
 
--- kornia_moons/feature.py
+++ kornia_moons/feature.py
@@ -3,7 +3,7 @@
 __all__ = ['laf_from_opencv_kpts', 'visualize_LAF', 'opencv_kpts_from_laf', 'laf_from_opencv_ORB_kpts',
            'laf_from_opencv_SIFT_kpts', 'opencv_SIFT_kpts_from_laf', 'opencv_ORB_kpts_from_laf',
            'cv2_matches_from_kornia', 'kornia_matches_from_cv2', 'to_numpy_image', 'epilines_to_start_end_points',
-           'draw_LAF_matches', 'draw_LAF_matches_from_result_dict']
+           'draw_LAF_matches', 'draw_LAF_matches_from_result_dict', 'draw_LAF_inliers_perspective_repjojected']
 
 # Cell
 import cv2
@@ -293,3 +293,114 @@ def draw_LAF_matches_from_result_dict(result_dict,
                                result_dict['tents_dict']['idxs'],
                         img1, img2, result_dict['inlier_mask'],
                         draw_dict, Fm, H)
+
+# Cell
+import kornia as K
+import kornia.feature as KF
+import torch
+def draw_LAF_inliers_perspective_repjojected(lafs1, lafs2, tent_idxs,
+                        img1, img2, inlier_mask = None,
+                        draw_dict={"inlier_color": (0.2, 1, 0.2),
+                               "reprojected_color": (0.2, 0.5, 1),
+                                "vertical": False},
+                        H: np.array = None,
+                        ax: Optional = None):
+    '''This function draws tentative matches and inliers given the homography H'''
+    inlier_mask = np.array(inlier_mask).reshape(-1)
+    img1 = to_numpy_image(img1)
+    img2 = to_numpy_image(img2)
+
+    h,w = img1.shape[:2]
+    h2,w2 = img2.shape[:2]
+
+    img2_tr = deepcopy(img2)
+
+    lafs1_in2 = KG.perspective_transform_lafs(torch.from_numpy(H).float()[None],
+                                              lafs1)
+    lafs2_in1 = KG.perspective_transform_lafs(torch.inverse(torch.from_numpy(H).float()[None]),
+                                              lafs2)
+    xy1 = KF.get_laf_center(lafs1).reshape(-1, 2)
+    xy2 = KF.get_laf_center(lafs2).reshape(-1, 2)
+
+    # If we have no axes, create one
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(20,10))
+
+    tent_corrs_in1 = torch.stack([xy1[tent_idxs[:,0]],
+                                  KF.get_laf_center(lafs2_in1).reshape(-1, 2)[tent_idxs[:,1]]])
+
+    tent_corrs_in2 = torch.stack([KF.get_laf_center(lafs1_in2).reshape(-1, 2)[tent_idxs[:,0]],
+                                  xy2[tent_idxs[:,1]]])
+
+    try:
+        vert = draw_dict['vertical']
+    except:
+        vert = False
+    if vert:
+        tent_corrs_in2[:,:,1]+=h # shift for the 2nd image
+    else:
+        tent_corrs_in2[:,:,0]+=w # shift for the 2nd image
+    # Prepraring canvas
+    if not vert:
+        if len(img1.shape) == 3:
+            new_shape = (max(h, h2), w + w2, img1.shape[2])
+        elif len(img1.shape) == 2:
+            new_shape = (max(h, h2), w + w2)
+    else:
+        if len(img1.shape) == 3:
+            new_shape = (h + h2,  max(w, w2), img1.shape[2])
+        elif len(img1.shape) == 2:
+            new_shape = (h + h2,  max(w, w2))
+    new_img = np.zeros(new_shape, type(img1.flat[0]))
+    # Place images onto the new image.
+    if not vert:
+        new_img[0:h, 0:w] = img1
+        new_img[0:h2, w:w + w2] = img2
+    else:
+        new_img[0:h, 0:w] = img1
+        new_img[h:h+h2, 0:w2] = img2
+
+    x1,y1 = KF.laf.get_laf_pts_to_draw(lafs1, 0)
+    x2,y2 = KF.laf.get_laf_pts_to_draw(lafs2, 0)
+
+    x1in2, y1in2 = KF.laf.get_laf_pts_to_draw(lafs1_in2, 0)
+    x2in1, y2in1 = KF.laf.get_laf_pts_to_draw(lafs2_in1, 0)
+
+    if vert:
+        y2+=h
+        y1in2+=h
+    else:
+        x2+=w
+        x1in2+=w
+
+    try:
+        ic = draw_dict['inlier_color']
+    except:
+        ic = None
+    if (ic is not None) and (inlier_mask is not None):
+        inlier_mask = inlier_mask > 0
+        ax.plot(tent_corrs_in1[..., inlier_mask, 0], tent_corrs_in1[...,inlier_mask, 1], color=ic)
+        ax.plot(tent_corrs_in2[..., inlier_mask, 0],
+                tent_corrs_in2[...,inlier_mask, 1], color=ic)
+        ax.plot(x1[:, tent_idxs[inlier_mask,0]], y1[:, tent_idxs[inlier_mask,0]], color=ic)
+        ax.plot(x2[:, tent_idxs[inlier_mask,1]], y2[:, tent_idxs[inlier_mask,1]], color=ic)
+    try:
+        rc = draw_dict['reprojected_color']
+    except:
+        rc = None
+    if rc is not None:
+        inlier_mask = inlier_mask > 0
+        ax.plot(x1in2[:, tent_idxs[inlier_mask,0]], y1in2[:, tent_idxs[inlier_mask,0]], color=rc)
+        ax.plot(x2in1[:, tent_idxs[inlier_mask,1]], y2in1[:, tent_idxs[inlier_mask,1]], color=rc)
+
+    # Finally clip the image
+    ax.imshow(new_img)
+    if not vert:
+        ax.set_xlim([0,w+w2])
+        ax.set_ylim([max(h,h2),0])
+        ax.margins(0,0)
+    else:
+        ax.set_xlim([0,max(w,w2)])
+        ax.set_ylim([h+h2, 0])
+        ax.margins(0,0)
+    return
